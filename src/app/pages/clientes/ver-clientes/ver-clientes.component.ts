07daafa1edc74f09b36ef86ca98418f58e7a6f4b
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-04-18 19:50:25 -0500

    ver clientes table

--- src/app/pages/clientes/ver-clientes/ver-clientes.component.ts
+++ src/app/pages/clientes/ver-clientes/ver-clientes.component.ts
@@ -1,4 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import {
+  ApexChart,
+  ChartComponent,
+
+} from 'ng-apexcharts';
+
+
+
+
+
+
+interface stats {
+  id: number;
+  time: string;
+  color: string;
+  title?: string;
+  subtext?: string;
+  link?: string;
+}
+
+export interface productsData {
+  id: number;
+  tipo_documento: string;
+  documento: string;
+  nombre: string;
+  direccion: string;
+  telefono: string;
+  foto_documento: string;
+}
+
+
+
+const ELEMENT_DATA: productsData[] = [
+  {
+    id: 1,
+    tipo_documento: "CC",
+    documento: "123456789",
+    nombre: "Jeon Jungkook",
+    direccion: "cl 57 #25-69",
+    telefono: "987654321",
+    foto_documento: 'assets/images/profile/user-1.jpg'   
+  },
+  {
+    id: 2,
+    tipo_documento: "CC",
+    documento: "123456789",
+    nombre: "Kim Taehyung",
+    direccion: "cl 57 #25-69",
+    telefono: "987654321",
+    foto_documento: 'assets/images/profile/user-2.jpg'   
+  },
+  {
+    id: 3,
+    tipo_documento: "CC",
+    documento: "123456789",
+    nombre: "Valentina Barbetty",
+    direccion: "cl 57 #25-69",
+    telefono: "987654321",
+    foto_documento: 'assets/images/profile/user-3.jpg'   
+  },
+  {
+    id: 4,
+    tipo_documento: "CC",
+    documento: "123456789",
+    nombre: "Jin",
+    direccion: "cl 57 #25-69",
+    telefono: "987654321",
+    foto_documento: 'assets/images/profile/user-4.jpg'   
+  },
+];
 
 @Component({
   selector: 'app-ver-clientes',
@@ -6,4 +76,14 @@ import { Component } from '@angular/core';
 })
 export class VerClientesComponent {
 
+
+
+  displayedColumns: string[] = ['Id', 'TipoDocumento', 'Documento', 'Nombre', 'Direccion', 'Telefono', 'FotoDocumento'];
+  dataSource = ELEMENT_DATA;
+
+
+  constructor() {
+    // sales overview chart
+  
+}
 }
