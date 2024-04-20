f57fb6dd65b4f2716cb65cf9beb8b346efe7f0e6
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-04-19 20:57:30 -0500

    conexion back

--- src/app/pages/clientes/agregar-cliente/agregar-cliente.component.ts
+++ src/app/pages/clientes/agregar-cliente/agregar-cliente.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -9,7 +12,14 @@ import Swal from 'sweetalert2';
 export class AgregarClienteComponent {
   selectedFile: string | ArrayBuffer | null = null;
 
-  constructor(private router: Router) { }
+  clienteForm = {
+    nombre: '',
+    tipo_documento: '',
+    documento: '',
+    direccion: '',
+    telefono: '',
+  }
+  constructor(private router: Router, private http: HttpClient) { }
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
@@ -21,18 +31,60 @@ export class AgregarClienteComponent {
       reader.readAsDataURL(file);
     }
   }
-  addClient() {
-    Swal.fire({
-      title: 'Cliente agregado con éxito',
-      text: '',
-      icon: 'success',
-      confirmButtonText: 'OK'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.router.navigate(['/clientes/ver-clientes']);
-      }
-    });
+  addClient(form: any) {
+    const data: any = {
+    
+    };
+    //
+    if(form.value.tipoDocumento === "Cédula de Ciudadanía"){
+      data.tipo_documento = "1";
+    } else if(form.value.documento === "Tarjeta de Identidad"){
+      data.tipo_documento = "2";
+    } else if(form.value.documento === "Cédula de Extranjería"){
+      data.tipo_documento = "3";
+    } else if(form.value.documento === "Pasaporte"){
+      data.tipo_documento = "4";
+    } else if(form.value.documento === "NIT"){
+      data.tipo_documento = "5";
+    } 
+    
+    data.nombre = form.value.nombre;
+    
+    data.documento = form.value.documento;
+    data.direccion = form.value.direccion;
+    data.telefono = form.value.telefono;
+
+    console.log("added", data)
+
+
+    this.http.post<any>('https://back-unisoft-lnv0.onrender.com/cliente/registerCliente', data)
+      .subscribe(
+        (response) => {
+          Swal.fire({
+            title: 'Cliente agregado con éxito',
+            text: '',
+            icon: 'success',
+            confirmButtonText: 'OK'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              this.router.navigate(['/clientes/ver-clientes']);
+            }
+          });
+        },
+        (error) => {
+          // Handle error response
+          console.error('Error adding client:', error);
+          Swal.fire({
+            title: 'Error',
+            text: 'Error adding client',
+            icon: 'error',
+            confirmButtonText: 'OK'
+          });
+        }
+      );
   }
+
+
 
   deleteSelectedPhoto() {
     this.selectedFile = null;
