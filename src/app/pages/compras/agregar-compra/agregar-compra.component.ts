d3fd8be068883bb2901289e04f05c490b108ad57
León Maya <70018777+TIGR4X@users.noreply.github.com>
2024-04-22 11:38:19 -0500

    Esqueleto Final Componente
    Se termina la realización del esqueleto HTML.
    
    Falta:
     - Resolver incidencias gráficas (ToDo)
     - Typescript con el funcionamiento de los módulos (1. Formulario de Agregar Compras, 3. Agregar Archivo Compraventa 3. Mostrar Info Compras, 4. Mostrar Info Clientes, 5. Routing a Registrar Cliente, 6. Generación de Factura)

--- src/app/pages/compras/agregar-compra/agregar-compra.component.ts
+++ src/app/pages/compras/agregar-compra/agregar-compra.component.ts
@@ -6,6 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './agregar-compra.component.html'
 })
 export class AgregarCompraComponent {
+  //Gestión Formulario Dispositivo
   selectedFormat: any = null;
   compraForm: FormGroup;
 
@@ -20,13 +21,30 @@ export class AgregarCompraComponent {
     });
   }
 
+  //Gestión Subida de Archivos
   onFormatSelected(event: any) {
     if (event.target.files && event.target.files[0]) {
       this.selectedFormat = event.target.files[0];
     }
   }
 
-  onSubmit() {
+  onSubmitCompra() {
+    if (this.compraForm.valid && this.selectedFormat) {
+      const formData = new FormData();
+      formData.append('formatoCompraVenta', this.selectedFormat);
+      Object.keys(this.compraForm.value).forEach(key => {
+        formData.append(key, this.compraForm.value[key]);
+      });
+
+      // Aquí puedes enviar los datos del formulario a tu servidor o servicio
+      // Por ejemplo: this.miServicio.agregarCompra(formData);
+    }
+  }
+  //Gestión Tabla
+  displayedColumns: string[] = ['IMEI', 'Marca de Teléfono', 'Procedencia', 'Modelo del Teléfono', 'Detalles del Teléfono', 'Valor de Compra'];
+
+  //Gestión Formulario Cliente (GET)
+  onSubmiCliente() {
     if (this.compraForm.valid && this.selectedFormat) {
       const formData = new FormData();
       formData.append('formatoCompraVenta', this.selectedFormat);
