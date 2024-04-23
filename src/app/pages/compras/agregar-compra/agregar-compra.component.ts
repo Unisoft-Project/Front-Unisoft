b135712a43ed29da380fc0067cf718d586ff8fb8
León Maya <70018777+TIGR4X@users.noreply.github.com>
2024-04-23 18:38:44 -0500

    Formulario Finalizado
    Se propone el código fuente del formulario de agregar dispositivo (Falta probar que sus respectivos endpoints funcionen correctamente), se realiza el routing con registrar usuario en caso de que no encuentre nada vía busqueda por número de identificación.

--- src/app/pages/compras/agregar-compra/agregar-compra.component.ts
+++ src/app/pages/compras/agregar-compra/agregar-compra.component.ts
@@ -13,8 +13,15 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 export class AgregarCompraComponent {
   selectedFile: string | ArrayBuffer | null = null; // Adjust type to File | null
   firebaseFile: File | null = null;
+  fireStorage: AngularFireStorage;
 
-  //* Gestión Formulario Compras (Entero)
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private storage: AngularFireStorage
+  ) {}
+
+  //* Estructura para Formulario Compras
   public compraForm = {
     imei: '',
     marca_telefono: '',
@@ -24,14 +31,23 @@ export class AgregarCompraComponent {
     valor_compra: 0
   };
 
+  //* Estructura para Busqueda Cliente
+  doc = '';
+  public clienteEncontrado = {
+    nombre: '',
+    tipo_documento: '',
+    documento: '',
+    direccion: '',
+    telefono: '',
+  };
 
   async addCompra(form: any) {
-    /*if (
+    const data: any = {};
+    if (
       !form.value.imei ||
       !form.value.marca_telefono ||
       !form.value.procedencia ||
       !form.value.modelo_telefono ||
-      !form.value.detalles ||
       !form.value.valor_compra
     ) {
       // Show Swal fire alert if any field is empty
@@ -42,7 +58,57 @@ export class AgregarCompraComponent {
         confirmButtonText: 'OK',
       });
       return;
-    }*/
+    }else {
+      if (this.firebaseFile) {
+        const file: File = this.firebaseFile as File;
+        const path = `docs/${form.value.documento}`;
+        this.fireStorage.upload(path, file);
+        const uploadTask = await this.fireStorage.upload(path, file);
+        const url = await uploadTask.ref.getDownloadURL();
+        console.log(url);
+        data.foto_documento = url;
+      }
+
+      // Set other client data
+      data.nombre = form.value.nombre;
+      data.documento = form.value.documento;
+      data.direccion = form.value.direccion;
+      data.telefono = form.value.telefono;
+
+      // TODO Revisar los campos y averiguar como concatenar info dispositivo + info cliente en un mismo paquete de datos
+
+      // Post client data to the server
+      this.http
+        .post<any>(
+          '',
+          // TODO Cambiar a endpoint correspondiente a agregar compras'https://back-unisoft-lnv0.onrender.com/cliente/registerCliente',
+          data
+        )
+        .subscribe(
+          (response) => {
+            Swal.fire({
+              title: 'Cliente agregado con éxito',
+              text: '',
+              icon: 'success',
+              confirmButtonText: 'OK',
+            }).then((result) => {
+              if (result.isConfirmed) {
+                this.router.navigate(['/clientes/ver-clientes']);
+              }
+            });
+          },
+          (error) => {
+            // Handle error response
+            console.error('Error adding client:', error);
+            Swal.fire({
+              title: 'Error',
+              text: 'Error adding client',
+              icon: 'error',
+              confirmButtonText: 'OK',
+            });
+          }
+        );
+    }
     }
 
   //* Subida de Formato CompraVenta
@@ -78,5 +144,20 @@ export class AgregarCompraComponent {
   };
 
   //Gestión GET Cliente
-  getCliente(){}
+  getCliente(documento: string){
+    const endpoint = `https://back-unisoft-1.onrender.com/cliente/listaClientes/documento/${documento}`;
+    this.http.get(endpoint).subscribe(
+      (response: any) => {
+        // Handle the response here
+        this.doc = documento;
+        console.log('response',response);
+        this.clienteEncontrado.nombre = response[0].nombre;
+        this.clienteEncontrado.direccion = response[0].direccion;
+        this.clienteEncontrado.telefono = response[0].telefono;
+      },(error) => {
+        // Handle errors here
+        console.error(error);
+      }
+    );
+  }
 }
