2fbe2405b9e1c1875f7fd81d8661f999490cad9e
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-05-21 13:55:11 -0500

    conf firebase, navbar y logo

--- src/app/layouts/full/sidebar/branding.component.ts
+++ src/app/layouts/full/sidebar/branding.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-branding',
   template: `
     <div class="branding">
-      <a href="/">
+      <a href="/dashboard">
         <img
           src="./assets/images/logos/Inventech.svg"
           class="align-middle m-2"
